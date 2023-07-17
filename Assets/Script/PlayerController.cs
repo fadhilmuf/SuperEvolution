bbf20922c19045547a6e15fb0b9b4002eff4e45b
fadhilmuf <112771091+fadhilmuf@users.noreply.github.com>
2023-07-17 20:58:38 +0700

    update dari AI enemy, bakal ngejar object dengan tag

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,18 +3,56 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-public Camera cam;
-public NavMeshAgent agent;
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    float score;
 
     void Update()
     {
-        if(Input.GetMouseButton(1))
+        FindNearestPointObject();
+    }
+
+    void FindNearestPointObject()
+    {
+        GameObject[] pointObjects = GameObject.FindGameObjectsWithTag("Point");
+
+        if (pointObjects.Length > 0)
         {
-            Ray movePosition = cam.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(movePosition, out var hitInfo))
+            GameObject nearestObject = pointObjects[0];
+            float nearestDistance = Vector3.Distance(transform.position, nearestObject.transform.position);
+
+            for (int i = 1; i < pointObjects.Length; i++)
             {
-                agent.SetDestination(hitInfo.point);
+                float distance = Vector3.Distance(transform.position, pointObjects[i].transform.position);
+
+                if (distance < nearestDistance)
+                {
+                    nearestObject = pointObjects[i];
+                    nearestDistance = distance;
+                }
             }
+
+            agent.SetDestination(nearestObject.transform.position);
+            agent.speed = 5f; // Set the agent's speed to make it run
+        }
+    }
+
+    void OnTriggerEnter(Collider other) //when object collide to trigger object
+    {
+        GameObject point = other.gameObject;
+        
+        switch (other.gameObject.tag)
+        {
+            case "Point":
+                point.SetActive(false);
+                score++;
+                Debug.Log(score);
+                if (score < 210f)
+                {
+                    transform.localScale *= 1.01f;
+                }
+            break;
         }
     }
 }
